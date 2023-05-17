bbace6e4b839490a6a72abf7ecd0379a3c75ac49
KyusokLee <kyusok1@naver.com>
2023-05-17 10:32:18 +0900

    [Change] - change window size and icon set

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import sys
 from PyQt6.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QTextEdit, QVBoxLayout
-
+from PyQt6.QtGui import QIcon
 class ChatWindow(QWidget):
     def __init__(self):
         super().__init__()
-
+        self.initUI()
+    
+    def initUI(self):
         layout = QVBoxLayout()
         self.text_edit = QTextEdit()
         layout.addWidget(self.text_edit)
@@ -13,6 +15,7 @@ class ChatWindow(QWidget):
         layout.addWidget(QLabel('Chat:'))
         self.setLayout(layout)
         self.setWindowTitle('Desktop Chat App')
+        self.setGeometry(800, 300, 400, 300)
         self.show()
     
 if __name__ == '__main__':
